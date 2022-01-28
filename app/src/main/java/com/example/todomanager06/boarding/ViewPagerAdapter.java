639769a827918dd59e320217011a5baeffbfbca4
qulesta1 <94120919+qulesta1@users.noreply.github.com>
2022-01-28 18:26:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/todomanager06/boarding/ViewPagerAdapter.java
@@ -0,0 +1,63 @@
+package com.example.todomanager06.boarding;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todomanager06.databinding.FragmentBoardBinding;
+import com.example.todomanager06.model.ViewPagerModel;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.ViewPagerHolder> {
+    ArrayList<ViewPagerModel> listPager;
+    ItemClickListener listener;
+
+    public ViewPagerAdapter(ArrayList<ViewPagerModel> listPager, ItemClickListener listener) {
+        this.listPager = listPager;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewPagerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewPagerHolder(FragmentBoardBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewPagerHolder holder, int position) {
+        holder.onBind(listPager.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listPager.size();
+    }
+
+    public class ViewPagerHolder extends RecyclerView.ViewHolder {
+        private FragmentBoardBinding binding;
+
+        public ViewPagerHolder(@NonNull FragmentBoardBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void onBind(ViewPagerModel model) {
+            binding.titleTv.setText(model.getTitle());
+            binding.descriptionTv.setText(model.getDescription());
+            binding.skipBtn.setText(model.getSkip());
+            binding.lottieAnim.setAnimation(model.getImage());
+            binding.skipBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.itemClick();
+                }
+            });
+
+        }}
+}
